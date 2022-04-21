4341658276c37b39a2e80977acda52e0aca15ac0
Bruce Zhu <brucezzhu@gmail.com>
2022-04-21 19:25:19 +1000

    made game.py and basic input handling

--- play.py
+++ play.py
@@ -3,7 +3,7 @@ from src.constants import SCREEN_W, SCREEN_H, FPS
 
 def main():
     pygame.init()
-    display = pygame.display.set_mode((SCREEN_W, SCREEN_H), 0, 32)
+    display = pygame.display.set_mode((SCREEN_W, SCREEN_H))
     font = pygame.font.SysFont("Arial", 24)
 
     running = True
@@ -12,6 +12,13 @@ def main():
 
     while running:
         delta = game_clock.tick(FPS)
+
+        events = pygame.event.get()
+
+        for e in events:
+            if e.type == pygame.QUIT:
+                running = False
+        break
 
 
 
